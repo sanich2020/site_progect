402fa466514e89ee95066d6848358342cbf02810
Andrey <andrey.biryukov07@mail.ru>
2024-04-22 18:01:20 +1100

    fifth_c

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(1000))
+    price = db.Column(db.Integer)
+    isActive = db.Column(db.Boolean, default=True)
+    txt = db.Column(db.Text)
 
 
 @app.route('/')
@@ -14,5 +25,4 @@ def about():
 if __name__ == '__main__':
     app.run(debug=True)
 
-    pass
 
