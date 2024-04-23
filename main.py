ea92b1befc93acf739b62c8d6c73b86e01132c00
Andrey <andrey.biryukov07@mail.ru>
2024-04-23 16:20:36 +1100

    six_c

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
